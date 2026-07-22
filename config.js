8e51fcd87ef1470adc809d8d10e5fe8c7e5dfcd0
1737917450-cpu <1737917450@qq.com>
2026-07-22 17:12:38 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+window.QUESTIONNAIRE_CONFIG = {
+  submissionEndpoint: "https://script.google.com/macros/s/AKfycbzi-KTOmysbt0ZfFIqsMwBS6HGDI1GMgfFiGcdH03BkF6sJYz8ACjKCAuDGm-KE_bLwvw/exec",
+  studyId: "ai_science_fund_proposal_evaluation_2026"
+};
